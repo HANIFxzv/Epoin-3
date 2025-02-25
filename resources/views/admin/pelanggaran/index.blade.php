3f86dd16df077f3b203152bbaa3b5b7652c8e1db
HANIFxzv <zidika16@gmail.com>
2025-02-25 13:42:26 +0700

    botttt

--- resources/views/admin/pelanggaran/index.blade.php
+++ resources/views/admin/pelanggaran/index.blade.php
@@ -45,7 +45,7 @@
             <td>
                 <form onsubmit="return confirm('Apakah Anda Yakin ?')" action="{{ route('pelanggaran.destroy', $pelanggaran->id) }}" method="POST">
                     <a href="{{ route('pelanggaran.edit', $pelanggaran->id) }}" class="btn btn-sm btn-primary">EDIT</a>
-                    @csrf
+                    @csrf   
                     @method('DELETE')
                     <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
                 </form>
